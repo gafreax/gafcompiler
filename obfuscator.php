73c7c25e1a1f9bb118020ac983fa09045a97e3c2
Gabriele Fontana <fontanag@devlnx.sba.bcc.it>
2014-07-19 12:04:59 +0200

    Aggiunto il semplice obfuscator

---
+++ obfuscator.php
@@ -0,0 +1,31 @@
+<?php
+static $logo = <<<EOF
+\033[1;32m
+   ___       __     ___        ___     ___                      _ _           
+  / _ \__ _ / _|   / _ \/\  /\/ _ \   / __\___  _ __ ___  _ __ (_) | ___ _ __ 
+ / /_\/ _` | |_   / /_)/ /_/ / /_)/  / /  / _ \| '_ ` _ \| '_ \| | |/ _ \ '__|
+/ /_\\ (_| |  _|  / ___/ __  / ___/  / /__| (_) | | | | | | |_) | | |  __/ |   
+\____/\__,_|_|   \/   \/ /_/\/      \____/\___/|_| |_| |_| .__/|_|_|\___|_|   
+                                                         |_|                  
+\033[0m
+\033[1;34mGabriele Fontana <gafreax@gmail.com>\033[0m
+
+EOF;
+
+echo $logo;
+if(!isset($argv[1])) {
+    echo 'Usage: php ' . __FILE__ . ' <filename.php>' . PHP_EOL;
+    exit;
+}
+$filename = $argv[1];
+if(!is_file($filename) || strpos($filename,'php') === false) {
+    echo $filename . ' is not a valid php file' . PHP_EOL;
+    exit;
+}
+$file = file_get_contents($argv[1]);
+$file = str_replace('<?php','',$file);
+$obfuscated = base64_encode($file);
+$exec = '<?php eval(base64_decode(\''.$obfuscated.'\')); ';
+echo $exec;
+
+
